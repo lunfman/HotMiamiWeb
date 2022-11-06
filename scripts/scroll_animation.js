1f9d8b114396930a80bab9908d2b849c0297e4a0
lunfman <smart275@gmail.com>
2022-11-06 22:34:35 +0200

    feat: add scroll_animation #24 - create animation in css - integrate classes with html - create script to detect elements - integrate script with html

---
+++ scripts/scroll_animation.js
@@ -0,0 +1,24 @@
+const isScrolledIntoView = (el) => {
+    // https://stackoverflow.com/questions/487073/how-to-check-if-element-is-visible-after-scrolling by korywka
+    let rect = el.getBoundingClientRect();
+    let elemTop = rect.top;
+    let elemBottom = rect.bottom;
+
+    // Only completely visible elements return true:
+    let isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+    // Partially visible elements return true:
+    //isVisible = elemTop < window.innerHeight && elemBottom >= 0;
+    return isVisible;
+}  
+
+const revealContent = () => {
+    let reveals = document.querySelectorAll(".reveal");
+    for (const reveal of reveals) {
+        isScrolledIntoView(reveal) &&  reveal.classList.add("active");
+        !isScrolledIntoView(reveal) &&  reveal.classList.remove("active");
+}
+}
+
+
+
+scroller.addEventListener('scroll', revealContent);
