8df90099736dcc447771e120c55cb84e70936291
lunfman <smart275@gmail.com>
2022-11-17 13:11:33 +0200

    refactor: nav script comment

--- scripts/nav_script.js
+++ scripts/nav_script.js
@@ -5,16 +5,18 @@ const menu = document.querySelector('.overlay')
 let open = false
 
 const openMenu = () => {
-    // function 
+    //  open pop up menu function
     open ? menu.style.width = '0%' : menu.style.width = '100%'
     toggle()
 }
 
 const toggle = () => {
+    // change state of the open to opposite bool
     open = !open
 }
 
 const closeMenu = () => {
+    // close pop up menu
     menu.style.width = '0%'
     toggle()
 }
