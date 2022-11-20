8dc2877f9a7dea874e89669702cbb9f681b5124a
lunfman <smart275@gmail.com>
2022-11-20 23:33:31 +0200

    feat: add new track and integrate track switch - add new tracks - add properties for the tracks - integrate playback and pause feature

--- scripts/music_play.js
+++ scripts/music_play.js
@@ -1,23 +1,42 @@
-const el = document.querySelector(".huero");
-const dot = document.querySelector(".dot");
-var audio = new Audio("./assets/audio/turf.mp3");
+const el = document.querySelectorAll(".album");
+const dot = document.querySelectorAll(".dot");
+let audio;
+music = ["turf", "pertubator", "byrne", "scattle"];
+let play, click, track;
+audio = new Audio(`./assets/audio/${"name"}.mp3`);
+console.log(audio.src);
 
-let play;
+const reset = () => {
+  el.forEach((album) => {
+    album.classList.remove("playing");
+    album.classList.add("stop");
+  });
+};
 
-function myFunction() {
-  if (!play) {
+function myFunction(element, name) {
+  //   reset();
+  audio.src = `./assets/audio/${name}.mp3`;
+  if (track != name) {
+    reset();
+    track = name;
     audio.play();
     play = true;
-    el.classList.add("playing");
-    el.classList.remove("stop");
+    element.classList.add("playing");
+    element.classList.remove("stop");
     dot.style.background = "red";
   } else {
+    reset();
     audio.pause();
-    play = false;
-    el.classList.remove("playing");
-    dot.style.background = "";
-    el.classList.add("stop");
+    // play = false;
+    // element.classList.remove("playing");
+    // dot.style.background = "";
+    // element.classList.add("stop");
   }
 }
 
-el.addEventListener("click", myFunction);
+el.forEach((album, index) => {
+  console.log(music[index]);
+  album.addEventListener("click", myFunction.bind(null, album, music[index]));
+});
+
+// el.addEventListener("click", myFunction);
